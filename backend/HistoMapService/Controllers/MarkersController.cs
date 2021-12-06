8515096f431022793e79d307ef4d309d691467c7
Jakub Kalina <kalinakuba6@gmail.com>
2021-12-06 21:03:00 +0100

    Dodanie testowych wartosci zwracanych przed endpoint

--- backend/HistoMapService/Controllers/MarkersController.cs
+++ backend/HistoMapService/Controllers/MarkersController.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using HistoMapService.DTOs.Requests;
+using HistoMapService.DTOs.Responses;
 using HistoMapService.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -21,8 +23,9 @@ namespace HistoMapService.Controllers
         [HttpPost]
         public async Task<IActionResult> GetMarkers([FromBody] GetMarkersRequest request)
         {
-            var response = await _markersService.GetMarkersAsync(request);
-            return Ok(response);
+            // var response = await _markersService.GetMarkersAsync(request);
+            return Ok("{\n    \"type\": \"FeatureCollection\",\n    \"features\": [\n        {\n            \"geometry\": {\n                \"type\": \"Point\",\n                \"coordinates\": [13.4, 42.35]\n            },\n            \"type\": \"Feature\",\n            \"properties\": {\n                \"count\": 9,\n                \"name\": \"Wiebestr./Huttenstr. (Berlin)\"\n            }\n        },\n        {\n            \"geometry\": {\n                \"type\": \"Point\",\n                \"coordinates\": [37.89935, 7.01931]\n            },\n            \"type\": \"Feature\",\n            \"properties\": {\n                \"count\": 3,\n                \"name\": \"Wiebestr./Huttenstr. (Berlin)\"\n            }\n        },\n        {\n            \"geometry\": {\n                \"type\": \"Point\",\n                \"coordinates\": [-10.21128, 39.02026]\n            },\n            \"type\": \"Feature\",\n            \"properties\": {\n                \"count\": 1,\n                \"name\": \"Wiebestr./Huttenstr. (Berlin)\"\n            }\n        }\n    ]\n}");
+            
         }
     }
 }
